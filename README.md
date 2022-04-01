a9e278a6041bcdf49715d864f75adeb366f1d79b
PleahMaCaka <kkjuh0905@gmail.com>
2022-04-01 23:14:24 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Preview
+
+### Example Code
+
+![code](./img/code.png)
+
+### Output logs
+
+![log](./img/log.png)
---
+++ test/test.ts
@@ -0,0 +1,8 @@
+import Logger, { Level } from "../src/logger";
+
+Logger.log("INFO", "Don't dwell on the past.")
+Logger.log(Level.INFO, "Believe in yourself.")
+Logger.log(Level.WARN, "Seize the day.")
+Logger.log(Level.ERROR, "Don't beat yourself up.")
+Logger.log(Level.DEBUG, "Past is just past.")
+Logger.log(Level.CRITICAL, "Life is a journey.")
